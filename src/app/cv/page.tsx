2d36d28ed829dcc44117a8f193afe1962e34af83
Alejandro Larreta Vzla <larreta_vzla@hotmail.com>
2025-05-04 17:11:05 -0700

    Primer commit, creacion de paginas base

---
+++ src/app/cv/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CVPage() {
+  return (
+    <div>
+      <h1>Pagina para el Curriculum</h1>
+    </div>
+  );
+}
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PortfolioPage() {
+  return (
+    <div>
+      <h1>Pagina para el Portafolio</h1>
+    </div>
+  );
+}
