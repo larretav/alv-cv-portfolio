fd5f64246e8e6c66da0eed04aa2a51b60ef9ae75
Alejandro Larreta Valenzuela <larreta_vzla@hotmail.com>
2025-05-23 16:29:05 -0700

    ExperienceItem agregado

--- src/components/typography/SectionTitle2.tsx
+++ src/components/typography/SectionTitle2.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 type Props = {
   text: string;
-  icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  icon?: React.ReactNode
 }
 
-export const SectionTitle2 = ({ text, icon: Icon }: Props) => {
+export const SectionTitle2 = ({ text, icon }: Props) => {
   return (
-    <h2 className="text-2xl font-bold flex items-center gap-2">
-      {Icon && <Icon className="size-6 text-primary dark:text-foreground" />}
-      {text}
-    </h2>
-  )
+    <div className="flex items-center mb-4 ">
+      <h2 className="text-2xl font-bold flex items-center gap-2">
+        <span className="text-primary dark:text-foreground/80 size-6">{icon}</span>
+        {text}
+      </h2>
+      <div className="ml-4 h-0.5 flex-grow bg-primary/20 dark:bg-foreground/20 rounded-full"></div>
+    </div>
+  );
 }
